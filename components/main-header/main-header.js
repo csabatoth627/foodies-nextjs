32da2e0cfdfe576f4327069304188a4c6647e80e
Csaba Toth <csabi627@gmail.com>
2024-07-18 11:56:02 +0200

    refactor: create nav-link component and style it

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,29 +1,31 @@
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
 import logoImg from "@/assets/logo.png";
-import classes from "./main-header.module.css"
+import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-backgroung";
+import NavLink from "./nav-link";
 
 export default function Mainheader() {
   return (
     <>
-    <MainHeaderBackground/>
-    <header className={classes.header}>
-      <Link className={classes.logo} href="/">
-        <Image src={logoImg} alt="A plate with food on it" priority/>
-        Nextlevel food
-      </Link>
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Browse Meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          Nextlevel food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href="/meals">Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href="/community">Foodies Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
     </>
   );
 }
